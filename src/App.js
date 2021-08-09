38472f80ef47043bfd5a51767833c316ff1b063b
Nachi Patil <npatil0821@gmail.com>
2021-08-08 21:49:57 -0400

    attempting to display list of added ingredients

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import React, { useState } from "react";
 import NewIngredient from "./components/NewIngredient";
 import IngredientList from "./components/IngredientList";
 
 function App() {
-  const ingredientList = [];
+  let [addedIngredients, setAddedIngredients] = useState([]);
 
   function addIngredientHandler(newIngredient) {
+    const ingredientList = addedIngredients;
     ingredientList.push(newIngredient);
+
+    setAddedIngredients(ingredientList);
   }
+
+  console.log(addedIngredients);
 
   return (
     <main>
       <NewIngredient onAddIngredient={addIngredientHandler} />
-      <IngredientList ingredients={ingredientList} />
+      <IngredientList ingredients={addedIngredients} />
     </main>
   );
 }
