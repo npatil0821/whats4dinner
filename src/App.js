86a9c07f15b4a05d210d9c154286717fce10430a
Nachi Patil <npatil0821@gmail.com>
2021-08-08 21:52:43 -0400

    added comments for some clarity

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import NewIngredient from "./components/NewIngredient";
 import IngredientList from "./components/IngredientList";
 
 function App() {
+  //stores an array of added ingredients
   let [addedIngredients, setAddedIngredients] = useState([]);
 
+  //used to add new ingredients to addedIngredients
   function addIngredientHandler(newIngredient) {
     const ingredientList = addedIngredients;
     ingredientList.push(newIngredient);
